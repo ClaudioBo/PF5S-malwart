ec3ee25c60ed2200349a0dd965cfdda1bf823e9b
Kledioz <claudiobo1122@gmail.com>
2020-12-07 21:33:52 -0700

    creds

--- connections/conn.php
+++ connections/conn.php
@@ -1,8 +1,8 @@
 <?php
-$servidor = "localhost";
-$baseDatos = "malwart";
-$usuarioBd = "root";
-$passwordBd = "";
+$servidor = "remotemysql.com";
+$baseDatos = "4jEzNtIWOQ";
+$usuarioBd = "4jEzNtIWOQ";
+$passwordBd = "fUKEIAtreZ";
 $mysqli = mysqli_connect($servidor, $usuarioBd, $passwordBd) or die(mysqli_error($mysqli));
 mysqli_query($mysqli, "SET NAMES 'utf8'");
 mysqli_select_db($mysqli, $baseDatos);
